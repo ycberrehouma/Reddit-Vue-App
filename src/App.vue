ec23261e89245bc86349be22cdbb29c5a32a4dca
Yassine Berrehouma <ycberrehouma@gmail.com>
2020-06-28 20:09:45 -0400

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,14 @@
         <h1>Reddit Posts from r/aww</h1>
         <div class="redditPosts" v-for="post in redditPosts">
             <h4>{{post.subreddit-name}}</h4>
-            <a href="https://www.reddit.com/r/aww/comments/{{post.id}}/{{post.title}}">{{ post.title }}</a>
-            <img :src="post.picture">
+            <reddit-list-container>
+              <a href="https://www.reddit.com/r/aww/comments/{{post.id}}/{{post.title}}">{{ post.title }}</a>
+              <img :src="post.picture">
+            </reddit-list-container>
+            <reddit-list-item>
+              <a href="https://www.reddit.com/r/aww/comments/{{post.id}}/{{post.title}}">{{ post.title }}</a>
+              <img :src="post.picture">
+            </reddit-list-item>
         </div>
         </div>
       </div>
@@ -12,81 +18,91 @@
       
       
     <script type = "text/javascript">
-    var j =0;
-        data () {
-          return {
-            redditPosts: []
-          }
-        },
-        methods: {
-          getInitialUsers () {// This function call the API, fetch and set some initial data ( 25 reddit posts) on page load:
-            for (var i = 0; i < 25; i++) {
-            var axios = require("axios");
-            axios.request({
-              url: "/oauth/token",
-              method: "get",
-              baseURL: "https://www.reddit.com/api/v1/collections/subreddit_collections",
-              auth: {
-                username: "LBEVzt5QdykXAQ", // This is the client_id
-                password: "-x0uJv47L50nVxc2q5CnvSpwZv0" // This is the client_secret
-              },
-              data: {
-                "grant_type": "client_credentials",
-                "scope": "public"    
-              }
-            }).then(response => {
-              if(response.status == 200 ) {
-                this.redditPosts.push(response.data.children[j]);
-                j++;  
-              }
-            });
-            .catch((error) => {
-              return Promise.reject(error)
-            })
+    Vue.component('reddit-list-container', {
+          data () {
+            return {
+              redditPosts: []
             }
-          } 
-
-         
-        beforeMount() {
-          this.getInitialUsers();
-        }
-        
-        
-        scroll (posts) {//This function calls the API and adds a new reddit post to the posts array when scrolling reaches the bottom of the page.
-            window.onscroll = () => {
-              let bottomOfWindow = document.documentElement.scrollTop + window.innerHeight === document.documentElement.offsetHeight;
-
-              if (bottomOfWindow) {
-                for (var i = 0; i < 25; i++) {
-                  axios.request({
-                  url: "/oauth/token",
-                  method: "get",
-                  baseURL: "https://www.reddit.com/api/v1/collections/subreddit_collections",
-                  auth: {
-                    username: "LBEVzt5QdykXAQ",
-                    password: "-x0uJv47L50nVxc2q5CnvSpwZv0"
-                  },
-                  data: {
-                    "grant_type": "client_credentials",
-                    "scope": "public"    
-                  }
-                  }).then(response => {
-                    if(response.status == 200 ) {
-                      this.redditPosts.push(response.data.children[j]); 
-                      j++; 
-                    }
-                  });
-                  .catch((error) => {
-                    return Promise.reject(error)
-                  })
-                } 
-              }
-            };
           },
-        },
-        mounted() {
-          this.scroll(this.redditPosts);
-        }
+          methods: {
+            getInitialUsers () {// This function call the API, fetch and set some initial data ( 25 reddit posts) on page load:
+              var j =0;
+              for (var i = 0; i < 25; i++) {
+              var axios = require("axios");
+              axios.request({
+                url: "/oauth/token",
+                method: "get",
+                baseURL: "https://www.reddit.com/api/v1/collections/subreddit_collections",
+                auth: {
+                  username: "LBEVzt5QdykXAQ", // This is the client_id
+                  password: "-x0uJv47L50nVxc2q5CnvSpwZv0" // This is the client_secret
+                },
+                data: {
+                  "grant_type": "client_credentials",
+                  "scope": "public"    
+                }
+              }).then(response => {
+                if(response.status == 200 ) {
+                  this.redditPosts.push(response.data.children[j]);
+                  j++;  
+                }
+              });
+              .catch((error) => {
+                return Promise.reject(error)
+              })
+              }
+            }
+          },  
+          beforeMount() {
+            this.getInitialUsers();
+          }
+      
+    }
+        
+    Vue.component('reddit-list-item', {
+          data () {
+            return {
+              redditPosts: []
+            }
+          },
+          methods: {    
+            scroll (posts) {//This function calls the API and adds a new reddit post to the posts array when scrolling reaches the bottom of the page.
+                var j = 25;
+                window.onscroll = () => {
+                  let bottomOfWindow = document.documentElement.scrollTop + window.innerHeight === document.documentElement.offsetHeight;
+
+                  if (bottomOfWindow) {
+                    for (var i = 0; i < 25; i++) {
+                      axios.request({
+                      url: "/oauth/token",
+                      method: "get",
+                      baseURL: "https://www.reddit.com/api/v1/collections/subreddit_collections",
+                      auth: {
+                        username: "LBEVzt5QdykXAQ",
+                        password: "-x0uJv47L50nVxc2q5CnvSpwZv0"
+                      },
+                      data: {
+                        "grant_type": "client_credentials",
+                        "scope": "public"    
+                      }
+                      }).then(response => {
+                        if(response.status == 200 ) {
+                          this.redditPosts.push(response.data.children[j]); 
+                          j++; 
+                        }
+                      });
+                      .catch((error) => {
+                        return Promise.reject(error)
+                      })
+                    } 
+                  }
+                };
+            },
+          }, 
+          mounted() {
+            this.scroll(this.redditPosts);
+          }
+    }
       </script>
       
       
